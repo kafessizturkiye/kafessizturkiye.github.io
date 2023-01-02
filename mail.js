8da0b18077221fea934206a5e37347494fb3c0cc
kafessizturkiye <121037975+kafessizturkiye@users.noreply.github.com>
2023-01-02 15:39:19 +0300

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,25 +1,3 @@
-const addresses = [
-    "info@larissahotels.com",
-    "deluxebelek@titanic.com.tr",
-    "info@sentidobellazure.com", 
-    "mado@mado.com.tr", 
-    "info@bigchefs.com.tr", 
-    "info@tavukdunyasi.com", 
-    "musterihizmetleri@kahvedunyasi.com", 
-    "ir@ulker.com", 
-    "bilgi@etietieti.com", 
-    "info@pinar.com.tr", 
-    "info@getir.com", 
-    "iletisim@migros.com.tr",
-    "destek@trendyol.com",  
-    "info@yemeksepeti.com",  
-    "musteri.hizmetleri@istegelsin.com", 
-    "kalite@sardunya.com", 
-    "info@mekasyemek.com", 
-    "info@aplus.web.tr"];
-
-const address_1 = addresses[Math.floor(Math.random() * addresses.length)]
-
-let mail = `mailto:${address_1}`
+let mail = `mailto:$iletisim@migros.com.tr`
 
 window.location.replace(mail);
