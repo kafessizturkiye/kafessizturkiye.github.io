46a7613c35e97577e6049f0504c5d9502083e5b2
kafessizturkiye <121037975+kafessizturkiye@users.noreply.github.com>
2023-12-14 18:18:13 +0300

    Update google.js

--- google.js
+++ google.js
@@ -1,10 +1,29 @@
-const addresses = "https://maps.app.goo.gl/fmAAFytrsfZZgZia6", "https://maps.app.goo.gl/ss76aTfvRWdjApMe6", "https://maps.app.goo.gl/7PFeWwcaPVtTggnA6",
+'use strict';
+const log = console.log
+log('Express server')
+
+const express = require('express')
+const app = express();
+
+const path = require('path');
+
+app.get('/', async function(req, res) {
+    
+    const links= ["https://maps.app.goo.gl/fmAAFytrsfZZgZia6", "https://maps.app.goo.gl/ss76aTfvRWdjApMe6", "https://maps.app.goo.gl/7PFeWwcaPVtTggnA6",
   "https://maps.app.goo.gl/Rp8TdLSJakzfwaZW8", "https://maps.app.goo.gl/QdzEPY2ZsgihyyCc9", "https://maps.app.goo.gl/33h4wJh3hPcM9rDA7", 
   "https://maps.app.goo.gl/ktTU3tDtrKJPAc419", "https://maps.app.goo.gl/fsDBw4VJLTXTWmK6A", "https://maps.app.goo.gl/c328NPRd9sUfX42Z9",
   "https://maps.app.goo.gl/MK3nzcRdzu7ukxrC9", "https://maps.app.goo.gl/a72Utejz5Mjnnpe3A", "https://maps.app.goo.gl/RgcQqXKs9g7nERCK6", 
   "https://maps.app.goo.gl/sPdit4i84ZvuUXU49", "https://maps.app.goo.gl/sPdit4i84ZvuUXU49", "https://maps.app.goo.gl/2WCZ4G3efsqfe5zU6",
-  "https://maps.app.goo.gl/xmHTh2D9RPnMxx518", "https://maps.app.goo.gl/33h4wJh3hPcM9rDA7","https://maps.app.goo.gl/QdzEPY2ZsgihyyCc9"];
+  "https://maps.app.goo.gl/xmHTh2D9RPnMxx518", "https://maps.app.goo.gl/33h4wJh3hPcM9rDA7","https://maps.app.goo.gl/QdzEPY2ZsgihyyCc9"]
+const link = links[Math.floor(Math.random() * links.length)]
 
-const googlelink = addresses[Math.floor(Math.random() * addresses.length)]
+    let open = link
+    res.redirect(open)
+})
 
-window.location.replace(googlelink);
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	log(`Listening on port ${port}...`)
+}) 
+
+////\r\nte
