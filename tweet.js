df5b313eae31487eae1cd1b3bda8f647daa07cae
kafessizturkiye <121037975+kafessizturkiye@users.noreply.github.com>
2025-04-21 16:39:27 +0300

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -1,3 +1,19 @@
 const address = "mailto:musterihizmetleri@altinmarkart.com?bcc=aksiyon2@kafessizturkiye.com"
 let link = address
-window.location.replace(link);
+window.location.replace(link);const addresses = [
+    "https://maps.app.goo.gl/unbeeRVSyPxZXHhb6",
+    "https://maps.app.goo.gl/77jGcJgGppJG5tcs8",
+    "https://maps.app.goo.gl/LdWp4m2Ai9NVZ2aN9",
+    "https://maps.app.goo.gl/9Z6qXi6zpMNMzdiTA",
+    "https://maps.app.goo.gl/dqrWaGsHuow8grHY6",
+    "https://maps.app.goo.gl/53qrTy4hb91QzFFs5"
+    
+    
+  
+    
+
+];
+
+const address_1 = addresses[Math.floor(Math.random() * addresses.length)]
+
+window.location.replace(address_1);
